0e2d6bb07785bfa9f0e4a84e2c100b44fdb2bd3f
agiannakakis <agiannakakis@deloitte.gr>
2024-08-14 17:28:00 +0300

    Navbar Burger

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,14 +3,13 @@ import { NavLink, useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../context/AuthContext';
 import { logout } from '../services/authService';
 import '../styles/Navbar.css';
-import '../styles/Navbar.css';
 
 const Navbar = ({ onAddButtonClick }) => {
   const { user, setUser } = useContext(AuthContext);
   const navigate = useNavigate();
   const location = useLocation();
   const [selectedCategory, setSelectedCategory] = useState(location.pathname);
-  
+  const [isMenuOpen, setIsMenuOpen] = useState(true); // Sidebar initially visible
 
   const handleLogout = () => {
     logout();
@@ -20,6 +19,7 @@ const Navbar = ({ onAddButtonClick }) => {
 
   const handleNavClick = (category) => {
     setSelectedCategory(category);
+    setIsMenuOpen(false); // Close menu on item click
   };
 
   const getButtonText = () => {
@@ -33,7 +33,7 @@ const Navbar = ({ onAddButtonClick }) => {
       case '/dashboard':
         return '+ Set Goals';
       case '/classrooms':
-        return ''; 
+        return '';
       default:
         return '';
     }
@@ -50,7 +50,7 @@ const Navbar = ({ onAddButtonClick }) => {
       case '/dashboard':
         return 'Please, set your goals through the button below!';
       case '/classrooms':
-        return ''; // Add a description if necessary
+        return '';
       default:
         return '';
     }
@@ -60,53 +60,67 @@ const Navbar = ({ onAddButtonClick }) => {
     onAddButtonClick(selectedCategory);
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav className="sidebar">
-      <h2 className="sidebar-heading">Elprof.</h2>
-      <ul className="nav flex-column">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/dashboard" onClick={() => handleNavClick('/dashboard')}>
-            Dashboard
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/students" onClick={() => handleNavClick('/students')}>
-            Students
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/professors" onClick={() => handleNavClick('/professors')}>
-            Professors
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/calendar" onClick={() => handleNavClick('/calendar')}>
-            Calendar
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/classrooms" onClick={() => handleNavClick('/classrooms')}>
-            Classrooms
-          </NavLink>
-        </li>
-        <div className="add-button-container">
-          <p>{getButtonTextDescription()}</p>
-          <button className="add-button" onClick={handleButtonClick}>{getButtonText()}</button>
-        </div>
-        <li className="nav-item mt-auto">
-          {user ? (
-            <a href="/login" className="nav-link logout-link" onClick={handleLogout}>
-              <img src="https://cdn-icons-png.flaticon.com/512/1828/1828466.png" alt="Logout Icon" className="logout-icon" />
-              Logout
-            </a>
-          ) : (
-            <NavLink className="nav-link" to="/login">
-              Login
+    <div>
+      {!isMenuOpen && (
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1828/1828859.png"
+          alt="Menu"
+          className="menu-icon"
+          onClick={toggleMenu}
+        />
+      )}
+      <nav className={`sidebar ${isMenuOpen ? 'open' : ''}`}>
+        <h2 className="sidebar-heading">Elprof.</h2>
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/dashboard" onClick={() => handleNavClick('/dashboard')}>
+              Dashboard
             </NavLink>
-          )}
-        </li>
-      </ul>
-    </nav>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/students" onClick={() => handleNavClick('/students')}>
+              Students
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/professors" onClick={() => handleNavClick('/professors')}>
+              Professors
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/calendar" onClick={() => handleNavClick('/calendar')}>
+              Calendar
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink className="nav-link" to="/classrooms" onClick={() => handleNavClick('/classrooms')}>
+              Classrooms
+            </NavLink>
+          </li>
+          <div className="add-button-container">
+            <p>{getButtonTextDescription()}</p>
+            <button className="add-button" onClick={handleButtonClick}>{getButtonText()}</button>
+          </div>
+          <li className="nav-item mt-auto">
+            {user ? (
+              <a href="/login" className="nav-link logout-link" onClick={handleLogout}>
+                <img src="https://cdn-icons-png.flaticon.com/512/1828/1828466.png" alt="Logout Icon" className="logout-icon" />
+                Logout
+              </a>
+            ) : (
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
