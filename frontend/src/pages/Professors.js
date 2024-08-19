dc4deaf5033ee1ddff211cffad1422383092e34f
agiannakakis <agiannakakis@deloitte.gr>
2024-08-19 15:22:26 +0300

    Students/Professors page size

--- frontend/src/pages/Professors.js
+++ frontend/src/pages/Professors.js
@@ -56,9 +56,9 @@ const Professors = () => {
   return (
     <div className="professor-container container-fluid">
       <div className="row">
-        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-        <div className="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-          <h1 className="h2 text-center">Καθηγητές</h1>
+        <main role="main" className="col-12 px-4">
+        <div className="d-flex justify-content-center align-items-center pt-3 pb-2 mb-3 border-bottom w-100">
+          <h1 className="h2 w-100 text-center">Καθηγητές</h1>
         </div>
 
 
--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -213,7 +213,7 @@ const Students = () => {
   return (
     <div className="students-container container-fluid">
       <div className="row">
-        <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
+        <main role="main" className="col-12 px-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
             <h1 className="students-title">Φοιτητές</h1>
             <div className="btn-toolbar mb-2 mb-md-0">
