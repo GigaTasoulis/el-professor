27c9aa1aa96cc4bb954baec2dc4a10453b74c4b1
agiannakakis <agiannakakis@deloitte.gr>
2024-08-19 12:35:36 +0300

    Fixed Professors' page responsiveness

--- frontend/src/pages/Professors.js
+++ frontend/src/pages/Professors.js
@@ -57,14 +57,10 @@ const Professors = () => {
     <div className="professor-container container-fluid">
       <div className="row">
         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
-          <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="h2">Καθηγητές</h1>
-            <div className="btn-toolbar mb-2 mb-md-0">
-              <button type="button" className="btn btn-sm btn-outline-secondary">
-                <i className="fa fa-filter"></i> Φίλτρα
-              </button>
-            </div>
-          </div>
+        <div className="d-flex justify-content-center flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+          <h1 className="h2 text-center">Καθηγητές</h1>
+        </div>
+
 
           <div className="table-responsive">
             <table className="table table-striped table-sm">
