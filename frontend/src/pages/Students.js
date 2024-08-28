b4a2271a847ed4cbaacff681ea8598a5433dbca1
agiannakakis <agiannakakis@deloitte.gr>
2024-08-28 12:33:58 +0300

    Fix-Students

--- frontend/src/pages/Students.js
+++ frontend/src/pages/Students.js
@@ -266,7 +266,7 @@ const Students = () => {
       <div className="row">
         <main role="main" className="col-12 px-4">
           <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-            <h1 className="students-title">Φοιτητές</h1>
+            <h1 className="students-title">Μαθητές</h1>
             <div className="btn-toolbar mb-2 mb-md-0">
               <button
                 type="button"
